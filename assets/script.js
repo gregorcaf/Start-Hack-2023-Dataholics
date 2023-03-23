5fa83a485e35ba7d9f8b126cddd10a58c9b995ac
gregorcaf <gregor.caf@gmail.com>
2023-03-23 18:43:41 +0100

    Implemented automatic changing of HTML elements

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,8 @@
-url = "http://localhost:8000/get-timeline-data?name=zan&month=february"
+let url = "http://localhost:8000/get-timeline-data?name=gordan&month=february"
+let totalEmissionsLow = 70
+let totalEmissionsHigh = 90
+let perTripConsumptionLow = 1
+let perTripConsumptionHigh = 2
 
 function initMap() {
     // The location of Uluru
@@ -29,9 +33,96 @@ function initMap() {
 
   }
 
-  function getData() {
-    fetch(url).then((response) => response.json()).then((data) => console.log(data));
-  }
+  // function getData() {
+  //   let fetched_data = 0
+  //
+  //   fetch(url).then((response) => response.json()).then((data) => console.log(data));
+  //   console.lo
+  // }
+
+function getData() {
+    let data;
+
+    fetch(url).then(function (u) {
+            return u.json();
+        }
+    ).then(function (json) {
+        data = JSON.parse(json);
+
+        let name = data["name"];
+        let rank = parseInt(data["friend_rank"].split(" / ")[0]);
+        let numPeople = parseInt(data["friend_rank"].split(" / ")[1]);
+        let totalEmissions = parseFloat(data["co2_absolute"]) / 1000;
+        let perTripConsumption = parseFloat(data["co2_average"]) / 1000;
+
+        console.log("name ", name)
+        console.log("rank ", rank) // DONE
+        console.log("num people ", numPeople)
+        console.log("total emissions ", totalEmissions) // DONE
+        console.log("per trip consumptions ", perTripConsumption) // DONE
+
+        let text_rank
+        let text_per_trip
+        let text_total_emissions
+
+        // rank
+        if (rank <= (numPeople / 3)) {
+            text_rank = "Munich area leaderboard: <span class=\"badge text-bg-success\">"
+        } else if (rank <= (numPeople - numPeople / 3)) {
+            text_rank = "Munich area leaderboard: <span class=\"badge text-bg-warning\">"
+        } else {
+            text_rank = "Munich area leaderboard: <span class=\"badge text-bg-danger\">"
+        }
+
+        text_rank += rank
+
+        if (rank == 1) {
+            text_rank += "st"
+
+        } else if (rank == 2) {
+            text_rank += "nd"
+        } else if (rank == 3) {
+            text_rank += "rd"
+        } else {
+            text_rank += "th"
+        }
+        text_rank += "</span>"
+
+
+        // total emissions
+        if (totalEmissions < totalEmissionsLow) {
+            text_total_emissions = "Total emissions: <span class=\"badge text-bg-success\">"
+        } else if (totalEmissions < totalEmissionsHigh) {
+            text_total_emissions = "Total emissions: <span class=\"badge text-bg-warning\">"
+        } else {
+            text_total_emissions = "Total emissions: <span class=\"badge text-bg-danger\">"
+        }
+
+        text_total_emissions += totalEmissions.toFixed(2);
+        text_total_emissions += " kg</span>"
+
+
+        // per trip consumption
+        if (perTripConsumption < perTripConsumptionLow) {
+            text_per_trip = "Per trip consumption: <span class=\"badge text-bg-success\">"
+        } else if (perTripConsumption < perTripConsumptionHigh) {
+            text_per_trip = "Per trip consumption: <span class=\"badge text-bg-warning\">"
+        } else {
+            text_per_trip = "Per trip consumption: <span class=\"badge text-bg-danger\">"
+        }
+
+        text_per_trip += perTripConsumption.toFixed(2);
+        text_per_trip += " kg</span>"
+
+        // change the data in HTML
+        document.getElementById("labelAreaLeaderboard").innerHTML = text_rank
+        document.getElementById("labelEcoFootprintNumber").innerHTML = text_total_emissions
+        document.getElementById("labelTotalEmissions").innerHTML = text_total_emissions
+        document.getElementById("labelPerTripConsumption").innerHTML = text_per_trip
+        }
+    )
+}
+
 
   let locations = [
     {name: 'A', lat: 48.1370339, lng: 11.5732385 },
