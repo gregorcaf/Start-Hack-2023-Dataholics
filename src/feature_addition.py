8818c6a520dde57a02e132f0ea3267662b0ea01a
Melvin Mathew <mpmic@live.com>
2023-03-23 13:32:07 +0100

    Unfinished code for Zan

--- src/feature_addition.py
+++ src/feature_addition.py
@@ -1,12 +1,20 @@
 import json
 
-
 def load_data(db_file):
     with open(db_file, "r") as f:
         data = json.load(f)
 
     generate_all_endpoint_data(data)
     return data
+
+
+def get_name_from_data(data: dict, name: str):
+    return data[name].capitalize()
+
+def get_co2_absolute_from_data(data:dict, name:str, month:str):
+    user_data = data[name]
+    user_data_for_month = user_data[f"2023_{month}"]["raw"]
+
 
 
 def generate_all_endpoint_data(data):
@@ -25,3 +33,4 @@ def get_all_end_locations(user_name, month, data):
     year = 2023
     month_str = f"{year}_{month}"
     return data[user_name][month_str]["features"]["all_end_locations"]
+
