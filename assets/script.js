c4a65d7d9f5f2f71ce786237930b496d53c80a2f
gregorcaf <gregor.caf@gmail.com>
2023-03-23 15:58:51 +0100

    Changed label inner HTML

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,45 @@
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: 48.1370339, lng: 11.5732385 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 12,
-      center: uluru,
+  // The location of Uluru
+  const uluru = { lat: 48.1370339, lng: 11.5732385 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: uluru,
+  });
+
+  let infowindow = new google.maps.InfoWindow();
+
+  let marker, i;
+  
+  for (i = 0; i < locations.length; i++) {  
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[i]['lat'], locations[i]['lng']),
+      map: map
     });
 
-    let infowindow = new google.maps.InfoWindow();
-
-    let marker, i;
-    
-    for (i = 0; i < locations.length; i++) {  
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i]['lat'], locations[i]['lng']),
-        map: map
-      });
-
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent(locations[i]['name']);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-    }
-
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      return function() {
+        infowindow.setContent(locations[i]['name']);
+        infowindow.open(map, marker);
+      }
+    })(marker, i));
   }
 
-  let locations = [
-    {name: 'A', lat: 48.1370339, lng: 11.5732385 },
-    {name: 'B', lat: 48.18, lng: 11.578 },
-    {name: 'C', lat: 48.12, lng: 11.7 },
-    {name: 'D', lat: 48.16, lng: 11.5 },
-    {name: 'E', lat: 48.19, lng: 11.6 }
-  ]
-  
-  window.initMap = initMap;
+}
+
+
+function initStatistics() {
+  document.getElementById("labelEcoFootprintNumber").innerHTML = "MARIBOR";
+  document.getElementById("labelEcoFootprintNumber").innerHTML = "METROPOLA";
+}
+
+
+let locations = [
+  {name: 'A', lat: 48.1370339, lng: 11.5732385 },
+  {name: 'B', lat: 48.18, lng: 11.578 },
+  {name: 'C', lat: 48.12, lng: 11.7 },
+  {name: 'D', lat: 48.16, lng: 11.5 },
+  {name: 'E', lat: 48.19, lng: 11.6 }
+]
+
+window.initMap = initMap;
