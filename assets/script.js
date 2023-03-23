4206acbb0def17685f26c4f78ec027c1582b1bc5
Stoick01 <work.gordan.prastalo@gmail.com>
2023-03-23 13:36:36 +0100

    Merged and fixed all

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,33 @@ function initMap() {
       zoom: 12,
       center: uluru,
     });
+
+    let infowindow = new google.maps.InfoWindow();
+
+    let marker, i;
+    
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i]['lat'], locations[i]['lng']),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i]['name']);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+
   }
+
+  let locations = [
+    {name: 'A', lat: 48.1370339, lng: 11.5732385 },
+    {name: 'B', lat: 48.18, lng: 11.578 },
+    {name: 'C', lat: 48.12, lng: 11.7 },
+    {name: 'D', lat: 48.16, lng: 11.5 },
+    {name: 'E', lat: 48.19, lng: 11.6 }
+  ]
   
   window.initMap = initMap;
